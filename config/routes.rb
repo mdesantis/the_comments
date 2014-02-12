4303574383ded27e71df923138a177f625d78f89
andrea longhi <andrea@spaghetticode.it>
2014-02-12 14:26:02 +0100

    add more admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,14 @@ module TheComments
 
   class AdminRoutes
     def call mapper, options = {}
+      mapper.member do
+        mapper.patch :to_published
+        mapper.patch :to_deleted
+        mapper.patch :to_draft
+        mapper.patch :to_spam
+      end
       mapper.collection do
+        mapper.get :index
         mapper.get :total_published
         mapper.get :total_deleted
         mapper.get :total_spam
